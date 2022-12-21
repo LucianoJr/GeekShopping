b8f7ce953befc5d1a22329dc8b47bcbd22da5748
Luciano Jr <luciano.juniorgiovani@gmail.com>
2022-12-20 21:01:33 -0300

    Adicionando o VO Product

---
+++ GekkShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GekkShopping.ProductAPI.Data.ValueObjects;
+using GekkShopping.ProductAPI.Model;
+
+namespace GekkShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps() 
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
