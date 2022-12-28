7f2fbf8d3a6a3e20b4bee441c6a35aa7ab996814
Luciano Jr <luciano.juniorgiovani@gmail.com>
2022-12-27 21:25:11 -0300

    Finalizando controller de Produto

---
+++ GekkShopping.ProductAPI/Migrations/20221228001701_SeedProductDataTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GekkShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 3L, "t-Shirt", "Lorem impsum", "https://github.com/leandrocgsi/erudio-microservices-dotnet6/tree/main/ShoppingImages", "Camiseta Preta", 56.9m },
+                    { 4L, "t-Shirt", "Lorem impsum", "https://github.com/leandrocgsi/erudio-microservices-dotnet6/tree/main/ShoppingImages", "Camiseta Branca", 56.9m },
+                    { 5L, "t-Shirt", "Lorem impsum", "https://github.com/leandrocgsi/erudio-microservices-dotnet6/tree/main/ShoppingImages", "Camiseta Verde", 56.9m },
+                    { 6L, "t-Shirt", "Lorem impsum", "https://github.com/leandrocgsi/erudio-microservices-dotnet6/tree/main/ShoppingImages", "Camiseta Star Wars", 56.9m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+        }
+    }
+}
--- GekkShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GekkShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -8,5 +8,45 @@ namespace GekkShopping.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Product>().HasData(new Product 
+            {
+                Id = 3,
+                Name = "Camiseta Preta",
+                Price = new decimal(56.9),
+                Description = "Lorem impsum",
+                ImageUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/tree/main/ShoppingImages",
+                CategoryName = "t-Shirt"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Camiseta Branca",
+                Price = new decimal(56.9),
+                Description = "Lorem impsum",
+                ImageUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/tree/main/ShoppingImages",
+                CategoryName = "t-Shirt"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Camiseta Verde",
+                Price = new decimal(56.9),
+                Description = "Lorem impsum",
+                ImageUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/tree/main/ShoppingImages",
+                CategoryName = "t-Shirt"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 6,
+                Name = "Camiseta Star Wars",
+                Price = new decimal(56.9),
+                Description = "Lorem impsum",
+                ImageUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/tree/main/ShoppingImages",
+                CategoryName = "t-Shirt"
+            });
+        }
     }
 }
