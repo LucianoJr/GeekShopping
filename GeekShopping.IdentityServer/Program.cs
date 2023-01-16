9619b669b65f630afa62ce8b92e8d661eefada94
Luciano Jr <luciano.juniorgiovani@gmail.com>
2023-01-16 20:09:23 -0300

    Adicionando configuração de usuario no Identity Server

--- GeekShopping.IdentityServer/Program.cs
+++ GeekShopping.IdentityServer/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using GeekShopping.IdentityServer.Model.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0, 29)))
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
